1323f0a68225749cee54ccc32bb15218981e44d1
Onyi Cho <onyicho@gmail.com>
2019-07-18 22:51:24 -0700

    Change webpack setting to load scss files, and add transpiler config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js', './src/main.scss'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -11,11 +11,39 @@ module.exports = {
     // (does not apply to resolving to loaders)
     modules: [
       "node_modules",
-      path.resolve(__dirname, "app")
+      path.resolve(__dirname, "src")
     ],
     // directories where to look for modules
-    extensions: [".js", ".json", ".jsx", ".css"],
+    extensions: [".js", ".json", ".jsx", ".scss",],
     // extensions that are used
   },
   devtool: "source-map", // enum
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules)/,
+        use: [
+          { loader: "style-loader"}, // creates style nodes from JS strings
+          { loader: "css-loader"}, // translates CSS into CommonJS
+          { 
+            loader: "sass-loader", // compiles Sass to CSS, using Node Sass by default
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
+        },
+      },
+    ]
+  }
 };
