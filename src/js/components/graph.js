c92cc503367d02e019cd0bf1450d7305a0106902
Onyi Cho <onyicho@gmail.com>
2019-07-21 22:54:22 -0700

    Add styling to tooltips div

--- src/js/components/graph.js
+++ src/js/components/graph.js
@@ -62,7 +62,6 @@ class Graph {
     let graphButtons = document.getElementsByClassName("graph-button");
      
     for(let i = 0; i < graphButtons.length; i++ ){
-      let that = this;
       let button = graphButtons[i];
       button.onclick = (e) => this.updateGraphType(e);
       
