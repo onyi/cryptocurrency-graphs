9bde36cc81f55f0981901d65b05a05c7edc4ab61
Onyi Cho <onyicho@gmail.com>
2019-07-22 04:59:16 -0700

    Remove unused code

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,7 @@
-import * as d3 from "d3";
-
-import coinType from './constants/coin_types';
-
-import { generateGraph } from './d3/generate_graph';
-
 import Graph from './components/graph';
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello World');
-
-  let params = {
-    'startDate': '2019/05/01',
-    'endDate': '2019/07/04',
-    'rows': 20,
-    "type": coinType.ethereum
-  }
-
 
   const graph = new Graph();
-  // graph.render();
-
-
-
 
 })
